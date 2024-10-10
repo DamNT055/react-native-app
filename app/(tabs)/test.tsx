e7138db1f35bca1bf16e468301c6a340f97e6fe2
dam.nguyen <dam.nguyen@lcsa.vn>
2024-10-10 13:16:10 +0700

    config nativewind

---
+++ app/(tabs)/test.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function TestSceen() {
+  return (
+    <View className="flex-1 items-center justify-center bg-slate-400">
+      <Text className="text-green-800 text-lg">Hello, NativeWind!</Text>
+    </View>
+  );
+}
